fbe2e7ca0ffc7fd82fb0b81cd0ae872c6065dbc3
Omkar Savant <omkar.savant@gmail.com>
2018-12-05 12:43:29 -0500

    split RRT grid into header file and source file, still not integrated with new occupancy grid

---
+++ src/rrt.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <unordered_map>
+#include <geometry_msgs/Point.h>
+#include <ros/ros.h>
+#include <geometry_msgs/Point.h>
+#include <visualization_msgs/Marker.h>
+
+using namespace std;
+
+class RRT {
+private:
+	int x_max;  
+	int y_max; 
+	vector<vector<int>> occu_grid;
+	vector<geometry_msgs::Point> nodes;
+
+	const int MAX_DIST; // The max distance between two nodes connected to each other
+
+	ros::NodeHandle n_ros;
+	ros::Publisher pub_marker;
+	visualization_msgs::Marker points, line_strip, line_list;
+
+	float dist(geometry_msgs::Point& p1, geometry_msgs::Point& p2); 
+
+	geometry_msgs::Point steer(geometry_msgs::Point& p1, geometry_msgs::Point& p2);
+
+	bool _InFreeSpace(geometry_msgs::Point& p);
+
+	bool collision_free(geometry_msgs::Point& p1, geometry_msgs::Point& p2);
+
+	geometry_msgs::Point random_point();
+
+	geometry_msgs::Point findNearest(geometry_msgs::Point &p);
+
+public:
+	RRT(vector<vector<int>> occugrid);
+	void plan_it(geometry_msgs::Point &p_start, geometry_msgs::Point &p_end);
+	void publish_it();
+};
